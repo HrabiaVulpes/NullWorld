4758b932cd802a78ab6af8e3d53ba57066398e31
HrabiaVulpes <hrabiavulpes@gmail.com>
2019-04-24 23:22:41 +0200

    duel improvements

--- src/main/java/scenes/Duel.java
+++ src/main/java/scenes/Duel.java
@@ -6,30 +6,31 @@ import combat_data.MoveTypes;
 import combat_data.States;
 
 public class Duel {
+    Effect p1Effect;
+    Effect p2Effect;
     private Combatant player1;
     private Combatant player2;
     private Integer distance = 2;
     private Integer maxDistance = 5;
-
 
     public Duel(Combatant player1, Combatant player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    public Combatant winner(){
+    Combatant winner() {
         if (player1.hitPoints > player2.hitPoints) return player1;
         if (player2.hitPoints > player1.hitPoints) return player2;
         return null;
     }
 
-    public Combatant looser(){
+    Combatant looser() {
         if (player1.hitPoints > player2.hitPoints) return player2;
         if (player2.hitPoints > player1.hitPoints) return player1;
         return null;
     }
 
-    public void processTurn() {
+    private void pickMovesStage() {
         if (player1.hitPoints <= 0 || player2.hitPoints <= 0) return;
 
         player1.setStates(player2.statesList, distance, player1.weapon.getLength());
@@ -37,7 +38,9 @@ public class Duel {
 
         player1.pickMove();
         player2.pickMove();
+    }
 
+    private void resolveMovesStage() {
         Effect p1Effect = player1.move.resolveAgainst(player2.move, player2.weapon.getLength(), distance);
         Effect p2Effect = player2.move.resolveAgainst(player1.move, player1.weapon.getLength(), distance);
 
@@ -45,41 +48,57 @@ public class Duel {
                 "\t" + player2.name + ": " + player2.move.getType().name() + "=" + p2Effect);
 
         if (p1Effect == Effect.HIT) player2.hitPoints -= (int) Math.round(player1.damageDealt());
-        if (p1Effect == Effect.CRIT) player2.hitPoints -= (int) Math.round(2*player1.damageDealt());
+        if (p1Effect == Effect.CRIT) player2.hitPoints -= (int) Math.round(2 * player1.damageDealt());
 
-        if (p2Effect == Effect.HIT) player1.hitPoints -= (int) Math.round(player2.damageDealt());;
-        if (p2Effect == Effect.CRIT) player1.hitPoints -= (int) Math.round(2*player2.damageDealt());
+        if (p2Effect == Effect.HIT) player1.hitPoints -= (int) Math.round(player2.damageDealt());
+        if (p2Effect == Effect.CRIT) player1.hitPoints -= (int) Math.round(2 * player2.damageDealt());
+    }
 
-
-        player1.learn(p1Effect, p2Effect, distance);
-        player2.learn(p2Effect, p1Effect, distance);
-
+    private void resolveDistanceStage() {
         if (player1.move.getType() == MoveTypes.BACK_AWAY && p2Effect != Effect.HIT && p2Effect != Effect.CRIT && distance < maxDistance)
             distance++;
         if (player2.move.getType() == MoveTypes.BACK_AWAY && p1Effect != Effect.HIT && p2Effect != Effect.CRIT && distance < maxDistance)
-            distance++;
-
-        if (player1.move.getType() == MoveTypes.KICK && distance < maxDistance)
-            distance++;
-        if (player2.move.getType() == MoveTypes.KICK && distance < maxDistance)
             distance++;
 
         if (player1.move.getType() == MoveTypes.CLOSE_IN && p2Effect != Effect.HIT && p2Effect != Effect.CRIT && distance > 0)
             distance--;
         if (player2.move.getType() == MoveTypes.CLOSE_IN && p1Effect != Effect.HIT && p2Effect != Effect.CRIT && distance > 0)
             distance--;
+    }
 
+    private void resolveStatesStage() {
+        //basic states from move
         player1.statesList.removeAll(player1.move.getRemovedStates());
-        player1.statesList.remove(States.ABOVE);
-        player1.statesList.addAll(player1.move.getAddedStates());
-
         player2.statesList.removeAll(player2.move.getRemovedStates());
+
+        //turned after side attack misses
+        if (player1.move.getAddedStates().contains(States.WEAPON_SIDE) && p1Effect == Effect.MISS)
+            player1.statesList.add(States.TURNED);
+        if (player2.move.getAddedStates().contains(States.WEAPON_SIDE) && p2Effect == Effect.MISS)
+            player2.statesList.add(States.TURNED);
+
+        //remove above - gravity
+        player1.statesList.remove(States.ABOVE);
         player2.statesList.remove(States.ABOVE);
+
+        //add basic states
+        player1.statesList.addAll(player1.move.getAddedStates());
         player2.statesList.addAll(player2.move.getAddedStates());
     }
 
-    public Combatant fightForRounds(int rounds){
-        for (int i = 0; i < rounds; i++){
+    private void processTurn() {
+        pickMovesStage();
+        resolveMovesStage();
+
+        player1.learn(p1Effect, p2Effect, distance);
+        player2.learn(p2Effect, p1Effect, distance);
+
+        resolveDistanceStage();
+        resolveStatesStage();
+    }
+
+    Combatant fightForRounds(int rounds) {
+        for (int i = 0; i < rounds; i++) {
             processTurn();
         }
         return winner();
